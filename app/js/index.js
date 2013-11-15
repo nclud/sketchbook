0c9f241b84226e28dc834da6fa8ffefcb0cc7c71
jared laser <laserjn@gmail.com>
2013-11-15 10:28:58 -0500

    add ability to close nav with esc key

--- app/js/index.js
+++ app/js/index.js
@@ -9,7 +9,6 @@ jQuery(document).ready(function() {
         $('.no-touch').remove();
     }
 
-
     // INITIALIZE SKROLLR
     var s = skrollr.init({ smoothScrolling: false });
 
@@ -18,6 +17,13 @@ jQuery(document).ready(function() {
     $('.menu-btn').click(function(e){
         e.preventDefault();
         $('#scroll-nav').toggleClass('active');
+    });
+
+    $(document).on('keyup', function(e) {
+        // esc key closes menu
+        if (e.keyCode == 27 ) {
+            $('#scroll-nav').removeClass('active');
+        }
     });
 
 });
