bde3875bdc5b4cfc94f27727c84f851117d5c1d4
jared laser <laserjn@gmail.com>
2013-11-20 13:33:58 -0500

    add imagesloaded so reel doesn't choke. seems to close #13

--- app/js/pages/beercamp.js
+++ app/js/pages/beercamp.js
@@ -1,8 +1,13 @@
-jQuery(document).ready(function() {
+// GLOBAL VARIABLES
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+var frameCount = 50;
+var scrollSpeed = 65;
+var imgPath = '/img/pages/beercamp/frames/frames_';
+var imgType = 'jpg';
+var frameArray = [];
 
-    // GLOBAL VARIABLES
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
+jQuery(document).ready(function() {
 
     // REMOVE UNNECESSARY / NONWORKING ELEMENTS ON MOBILE/TOUCH DEVICES
     if (navigator.userAgent.match(/mobile/i)) {
@@ -18,101 +23,6 @@ jQuery(document).ready(function() {
         $('#mario').attr('data-96p-bottom','');
         $('#mario').attr('data-97p-bottom','');
     }
-
-    // IMAGE PRELOADING FOR ANIMATION
-    var frameCount = 50;
-    var scrollSpeed = 65;
-    var imgPath = '/img/pages/beercamp/frames/frames_';
-    var imgType = 'jpg';
-    var frameArray = [];
-
-    if (windowWidth > 768 && windowWidth < 1100) {
-        scrollSpeed = 45;
-    }
-
-    function padNumbers(number) {
-        if (number<=99) {
-            number = ("0"+number).slice(-2);
-        }
-        return number;
-    }
-    if (windowWidth > 640) {
-        for (var i=0; i<frameCount; i++) {
-            frameArray[i]=imgPath+padNumbers(i+1)+'.'+imgType;
-        }
-    }
-
-    // FRAMES ON SCROLL
-    if (windowWidth > 640) {
-        $('#scroll-animation').reel({
-            cursor: 'default',
-            draggable: false,
-            loops: false,
-            frames: frameCount,
-            images: frameArray
-        });
-
-        $('#scroll-animation-reel img').each(function(){
-            $(this).removeAttr('width');
-            $(this).removeAttr('height');
-        });
-    }
-
-
-    // SET HEIGHT / FIXED POSITIONING WITHOUT ACTUALLY FIXING POSITIONING
-    var designContainer = (windowHeight + 625 + (frameCount * scrollSpeed) + 200);
-    var designFinal = designContainer - windowHeight;
-
-    $('#design-container').css('height',designContainer);
-    $('#design').css('height',windowHeight);
-
-    if (!navigator.userAgent.match(/mobile/i)) {
-        $('#design').attr('data-0-top','');
-        $('#design').attr('data-bottom-bottom','');
-
-        $('#right-diagram').attr('data-0-top','');
-        $('#right-diagram').attr('data-bottom-bottom','');
-    }
-    if (navigator.userAgent.match(/mobile/i) && windowWidth > 640) {
-        $('#design').attr('data-0-top','top: 0px;');
-        $('#design').attr('data--' + designFinal + '-top','top: ' + designFinal + 'px;');
-    }
-    if (navigator.userAgent.match(/mobile/i) && windowWidth > 768) {
-        var devFinal = $('#develop').height() - ($('#right-diagram').outerHeight()/2);
-        $('#right-diagram').attr('data-0-top','top: 0px;');
-        $('#right-diagram').attr('data--' + devFinal + '-top','top: ' + devFinal + 'px;');
-    }
-
-    // INITIALIZE SKROLLR
-    var heightToggle = true;
-
-    var s = skrollr.init({
-        smoothScrolling: false,
-        mobileDeceleration: 1,
-        render: function(data) {
-            if ( document.getElementById('design') ) {
-              if (navigator.userAgent.match(/mobile/i) && windowWidth > 640) {
-                  var designFrames = $('#design').css('top').replace(/[^-\d\.]/g, '');
-                  var animationStart = (designFrames - 625);
-                  $('#scroll-animation').reel('frame', (animationStart / scrollSpeed));
-              }
-              else if (!navigator.userAgent.match(/mobile/i)) {
-                  var designFrames = $('#design').offset();
-                  var designTop = $('#design-container').offset();
-                  var animationStart = ((designFrames.top-625)-designTop.top)
-                  $('#scroll-animation').reel('frame', (animationStart / scrollSpeed));
-              }
-
-              var videoTop = $('#video-screen').offset().top;
-              if (heightToggle && ((data.curTop + windowHeight) >= (videoTop - 75)) && !navigator.userAgent.match(/mobile/i)) {
-                  $('#video-screen video').append('<source src="/img/pages/beercamp/video/beercamp.mp4" type="video/mp4"/><source src="/img/pages/beercamp/video/beercamp.webm" type="video/webm"/>');
-                  heightToggle = false;
-              }
-            }
-        }
-    });
-
-    s.refresh($('#skrollr-body'));
 
     // LAYERS OF DEV WITH INFO
     $('.layer-description').css('display','none');
@@ -135,7 +45,7 @@ jQuery(document).ready(function() {
 
 
     // SCROLL TO TOP
-    $('#scroll-to-top').click(function(){
+    $('#scroll-to-top').on('click', function(){
         s.setScrollTop(0);
         return false;
     });
@@ -171,6 +81,100 @@ jQuery(document).ready(function() {
             });
         }
     });
+});
+
+imagesLoaded( document.querySelector('#skrollr-body'), function( instance ) {
+
+    // IMAGE PRELOADING FOR ANIMATION
+    if (windowWidth > 768 && windowWidth < 1100) {
+        scrollSpeed = 45;
+    }
+
+    function padNumbers(number) {
+        if (number<=99) {
+            number = ("0"+number).slice(-2);
+        }
+        return number;
+    }
+    if (windowWidth > 640) {
+        for (var i=0; i<frameCount; i++) {
+            frameArray[i]=imgPath+padNumbers(i+1)+'.'+imgType;
+        }
+    }
+
+    // FRAMES ON SCROLL
+    if (windowWidth > 640) {
+        $('#scroll-animation').reel({
+            cursor: 'default',
+            draggable: false,
+            loops: false,
+            preload: 'linear',
+            frames: frameCount,
+            images: frameArray
+        });
+
+        $('#scroll-animation-reel img').each(function(){
+            $(this).removeAttr('width');
+            $(this).removeAttr('height');
+        });
+    }
+
+
+    // SET HEIGHT / FIXED POSITIONING WITHOUT ACTUALLY FIXING POSITIONING
+    var designContainer = (windowHeight + 625 + (frameCount * scrollSpeed) + 200);
+    var designFinal = designContainer - windowHeight;
+
+    $('#design-container').css('height',designContainer);
+    $('#design').css('height',windowHeight);
+
+    if (!navigator.userAgent.match(/mobile/i)) {
+        $('#design').attr('data-0-top','');
+        $('#design').attr('data-bottom-bottom','');
+
+        $('#right-diagram').attr('data-0-top','');
+        $('#right-diagram').attr('data-bottom-bottom','');
+    }
+    if (navigator.userAgent.match(/mobile/i) && windowWidth > 640) {
+        $('#design').attr('data-0-top','top: 0px;');
+        $('#design').attr('data--' + designFinal + '-top','top: ' + designFinal + 'px;');
+    }
+    if (navigator.userAgent.match(/mobile/i) && windowWidth > 768) {
+        var devFinal = $('#develop').height() - ($('#right-diagram').outerHeight()/2);
+        $('#right-diagram').attr('data-0-top','top: 0px;');
+        $('#right-diagram').attr('data--' + devFinal + '-top','top: ' + devFinal + 'px;');
+    }
+
+
+    // INITIALIZE SKROLLR
+    var heightToggle = true;
+
+    var s = skrollr.init({
+        smoothScrolling: false,
+        mobileDeceleration: 1,
+        render: function(data) {
+            if ( document.getElementById('design') ) {
+              if (navigator.userAgent.match(/mobile/i) && windowWidth > 640) {
+                  var designFrames = $('#design').css('top').replace(/[^-\d\.]/g, '');
+                  var animationStart = (designFrames - 625);
+                  $('#scroll-animation').reel('frame', (animationStart / scrollSpeed));
+              }
+              else if (!navigator.userAgent.match(/mobile/i)) {
+                  var designFrames = $('#design').offset();
+                  var designTop = $('#design-container').offset();
+                  var animationStart = ((designFrames.top-625)-designTop.top)
+                  $('#scroll-animation').reel('frame', (animationStart / scrollSpeed));
+              }
+
+              var videoTop = $('#video-screen').offset().top;
+              if (heightToggle && ((data.curTop + windowHeight) >= (videoTop - 75)) && !navigator.userAgent.match(/mobile/i)) {
+                  $('#video-screen video').append('<source src="/img/pages/beercamp/video/beercamp.mp4" type="video/mp4"/><source src="/img/pages/beercamp/video/beercamp.webm" type="video/webm"/>');
+                  heightToggle = false;
+              }
+            }
+        }
+    });
+
+    s.refresh($('#skrollr-body'));
     s.refresh($('footer'));
 
 });
