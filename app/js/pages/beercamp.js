6db9dcc7eac10d44fbdef3248def3d645eb96fcd
Kyle Conrad <kyle.matthew.conrad@gmail.com>
2013-12-10 17:11:11 -0500

    Spacing/tabbing

--- app/js/pages/beercamp.js
+++ app/js/pages/beercamp.js
@@ -133,27 +133,29 @@ imagesLoaded( document.querySelector('#skrollr-body'), function( instance ) {
         smoothScrolling: false,
         mobileDeceleration: 1,
         render: function(data) {
-	  if($('header').hasClass('beercamp')){
-            if (isMobile && biggerThanPhone) {
-                var tabletDesignFrames = $('#beercamp_design').css('top').replace(/[^-\d\.]/g, '');
-                var tabletAnimationStart = (tabletDesignFrames - 625);
-                var tabletFrames = (tabletAnimationStart / bc.scrollSpeed);
-                $('#beercamp_scroll-animation').reel('frame', tabletFrames);
-            }
-            if (!isMobile) {
-                var designFrames = $('#beercamp_design').offset();
-                var designTop = $('#beercamp_design-container').offset();
-                var animationStart = ((designFrames.top-625)-designTop.top);
-                var desktopFrames = (animationStart / bc.scrollSpeed);
-                $('#beercamp_scroll-animation').reel('frame', desktopFrames);
 
-                var videoTop = $('#beercamp_video-screen').offset().top;
-                if (heightToggle && ((data.curTop + windowHeight) >= (videoTop - 75))) {
-                    $('#beercamp_video-screen video').append('<source src="/img/pages/beercamp/video/beercamp.mp4" type="video/mp4"/><source src="/img/pages/beercamp/video/beercamp.webm" type="video/webm"/>');
-                    heightToggle = false;
+            if($('header').hasClass('beercamp')){
+                if (isMobile && biggerThanPhone) {
+                    var tabletDesignFrames = $('#beercamp_design').css('top').replace(/[^-\d\.]/g, '');
+                    var tabletAnimationStart = (tabletDesignFrames - 625);
+                    var tabletFrames = (tabletAnimationStart / bc.scrollSpeed);
+                    $('#beercamp_scroll-animation').reel('frame', tabletFrames);
+                }
+                if (!isMobile) {
+                    var designFrames = $('#beercamp_design').offset();
+                    var designTop = $('#beercamp_design-container').offset();
+                    var animationStart = ((designFrames.top-625)-designTop.top);
+                    var desktopFrames = (animationStart / bc.scrollSpeed);
+                    $('#beercamp_scroll-animation').reel('frame', desktopFrames);
+
+                    var videoTop = $('#beercamp_video-screen').offset().top;
+                    if (heightToggle && ((data.curTop + windowHeight) >= (videoTop - 75))) {
+                        $('#beercamp_video-screen video').append('<source src="/img/pages/beercamp/video/beercamp.mp4" type="video/mp4"/><source src="/img/pages/beercamp/video/beercamp.webm" type="video/webm"/>');
+                        heightToggle = false;
+                    }
                 }
             }
-	  }
+
         }
     });
     s.refresh($('#skrollr-body'));
