245bf8cfa9faba7970043355f0aac5c419c16dc1
Kyle Conrad <kyle@kyleconrad.com>
2014-08-25 15:39:57 -0400

    Rebuild in static files

--- prod/js/footer.js
+++ prod/js/footer.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+	var windowWidth = $(window).width(),
+		windowHeight = $(window).height();
+	var isMobile = navigator.userAgent.match(/mobile/i);
 
+
+	// INITIALIZE FASTCLICK
+    FastClick.attach(document.body);
+
+
+	// WIDOW CONTROL
+	if (windowWidth > 640) {
+		$('h1, h2, h3, li, p, figcaption').each(function() {
+		    $(this).html($(this).html().replace(/\s((?=(([^\s<>]|<[^>]*>)+))\2)\s*$/,'&nbsp;$1'));
+		});
+	}
+
+
+	// RESPONSIVE VIDEO EMBEDS
+    $('article, header').fitVids();
 });
